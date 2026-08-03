20c2e6a66a5f0ac83e2d6fe204dda436e4ca4955
Fuma078 <fam0075552@gmail.com>
2026-08-03 14:39:53 +0700

    6.3 commit fix*

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -23,10 +23,14 @@ defineProps<{
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
+  transition:
+    background-color 0.2s ease,
+    box-shadow 0.2s ease;
 }
 .user-card:hover {
+  background-color: #f0f0f0;
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 3px 12px 0 rgba(100, 100, 100, 0.3);
 }
 .user-link {
   text-decoration: none;
