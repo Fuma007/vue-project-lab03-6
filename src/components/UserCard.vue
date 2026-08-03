bca6278b7cf10634fe49d4962c4fc05930078970
Fuma078 <fam0075552@gmail.com>
2026-08-03 14:34:50 +0700

    6.2 commit*

---
+++ src/components/UserCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { User } from '@/types'
+
+defineProps<{
+  user: User
+}>()
+</script>
+
+<template>
+  <RouterLink class="user-link" :to="{ name: 'user-detail-view', params: { id: user.id } }">
+    <div class="user-card">
+      <h2>{{ user.name }}</h2>
+      <p>{{ user.email }}</p>
+      <p>{{ user.company.name }}</p>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.user-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.user-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+.user-link {
+  text-decoration: none;
+  color: #2c3e50;
+}
+</style>
